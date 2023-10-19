2b0414d65ed91ee679eb27200d1ef2386ab64302
Taha Yıldırım <59919927+thyldrm@users.noreply.github.com>
2023-10-19 13:29:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const { Octokit } = require("@octokit/rest");
 const yaml = require("js-yaml");
 
 const {
-  countAndGroupByTitle,
-  convertToHHMMSS,
-  getScore,
-  countBySeverity,
-  htmlCode,
   findWeaknessTitles,
-  newIssue,
-  allIssue,
   failedArgs,
+  sign,
+  check,
+  create,
+  start,
+  status,
+  result,
 } = require("./utils");
 
 let token = process.env.ACCESS_TOKEN;
@@ -26,8 +25,8 @@ const orgname = process.env.ORGNAME;
 const repoName = github.context.repo.repo;
 const repoOwner = github.context.repo.owner;
 const type = github.context.payload.repository.private ? "private" : "public";
-const parts = github.context.ref.split("/");
-let branch = parts.at(-1);
+const parts = github.context.ref?.split("/");
+let branch = parts?.at(-1);
 let repoId = github.context.payload.repository.id;
 
 let pr;
@@ -43,393 +42,227 @@ const commitMessage =
   github.context.payload?.head_commit?.message ||
   github.context.payload?.pull_request?.title;
 
-console.log("repoName", repoName);
-console.log("repoOwner", repoOwner);
-console.log("pr", pr?.number);
-console.log("type", type);
-console.log("commitId", commitId);
-console.log("committer", committer);
-console.log("commitMessage", commitMessage);
-console.log("branch", branch);
-console.log("repoId", repoId);
-
-console.log("PAYLOAD ------> ,,,,,,, ___>", github.context.payload)
-console.log("ContECTrt ------> ,,,,,,, ___>", github.context)
-
 const failedArgsInput = core.getInput("FAILED_ARGS");
 const failedArgsParsed = yaml.load(failedArgsInput);
 const output = failedArgs(failedArgsParsed);
 
-
 if (output.automerge === undefined) output.automerge = false;
 if (output.condition === undefined) output.condition = "AND";
+if (output.sync_scan === undefined) output.sync_scan = true;
 
 const octokit = new Octokit({
   auth: githubtoken,
 });
 
-let scanProcess, cancellation;
+let scanProcess, authToken, checked;
 
-const startScan = async () => {
-  try {
-    let responseToken;
-    if (!token && username && password) {
-      const b64uidpss = btoa(`${username}:${password}`);
-      const authorization = `Basic ${b64uidpss}`;
-      responseToken = await axios.post(`${ctServer}/api/signin`, {
-        client_id: username,
-        client_secret: password,
-      });
-    }
-    responseToken?.data.access_token
-      ? (token = responseToken.data.access_token)
-      : token;
-    let scanStarting;
-    try {
-      scanStarting = await axios.post(
-        `${ctServer}/api/integration/github/start`,
-        {
-          project: repoName,
-          branch: branch,
-          account: repoOwner,
-          type: type,
-          githubtoken: githubtoken,
-          id: repoId,
-          action: true,
-          commitId: commitId
-        },
-        {
-          headers: {
-            Authorization: token,
-            "x-ct-organization": "codethreat",
-          },
-        }
-      );
-    } catch (error) {
-      throw new Error(error.response.data.message)
-    }
-    return scanStarting;
-  } catch (error) {
-    core.setFailed(error)
+const signIn = async () => {
+  if (token && (!username || !password)) {
+    authToken = token;
+  } else if (username && password) {
+    authToken = await sign(ctServer, username, password);
+  } else {
+    core.setFailed("Please enter username and password or token.");
   }
 };
 
-let progressData = [];
-let progressSeverity = [];
+const checkProject = async () => {
+  return await check(ctServer, repoName, authToken, orgname);
+};
 
-const awaitScan = async (sid) => {
+const createProject = async () => {
+  return await create(
+    ctServer,
+    repoName,
+    branch,
+    repoOwner,
+    type,
+    githubtoken,
+    repoId,
+    authToken,
+    orgname
+  );
+};
+
+const startScan = async () => {
+  return await start(
+    ctServer,
+    repoName,
+    branch,
+    repoOwner,
+    type,
+    githubtoken,
+    repoId,
+    commitId,
+    committer,
+    commitMessage,
+    authToken,
+    orgname
+  );
+};
+
+const scanStatus = async (sid) => {
   try {
-    scanProcess = await axios.get(`${ctServer}/api/scan/status/${sid}`, {
-      headers: {
-        Authorization: token,
-        "x-ct-organization": "codethreat",
-      },
-    });
-    progressData.push(scanProcess.data.progress_data.progress);
-    progressSeverity.push(scanProcess.data.severities);
-    if (scanProcess.data.state !== "end") {
-      console.log(`Scanning... ` + `%` + progressData[progressData.length - 1]);
-      progressSeverity[progressSeverity.length - 1].critical
-        ? progressSeverity[progressSeverity.length - 1].critical
-        : (progressSeverity[progressSeverity.length - 1].critical = 0);
-      progressSeverity[progressSeverity.length - 1].high
-        ? progressSeverity[progressSeverity.length - 1].hgih
-        : (progressSeverity[progressSeverity.length - 1].high = 0);
-      progressSeverity[progressSeverity.length - 1].medium
-        ? progressSeverity[progressSeverity.length - 1].medium
-        : (progressSeverity[progressSeverity.length - 1].medium = 0);
-      progressSeverity[progressSeverity.length - 1].low
-        ? progressSeverity[progressSeverity.length - 1].low
-        : (progressSeverity[progressSeverity.length - 1].low = 0);
+    scanProcess = await status(ctServer, sid, authToken, orgname);
+    if (scanProcess.state === "failure") {
+      core.setFailed("Scan Failed.");
+      throw new Error("Scan Failed.");
+    }
+    if(output.sync_scan){
+      console.log("Scan started successfuly.")
+      return;
+    }
+    if (scanProcess.state !== "end") {
       core.warning(
-        "\n" +
-          "Critical : " +
-          progressSeverity[progressSeverity.length - 1].critical +
-          "\n" +
-          "High : " +
-          progressSeverity[progressSeverity.length - 1].high +
-          "\n" +
-          "Medium : " +
-          progressSeverity[progressSeverity.length - 1].medium +
-          "\n" +
-          "Low : " +
-          progressSeverity[progressSeverity.length - 1].low +
+        "Scanning... " +
+          "%" +
+          scanProcess.progress +
+          " - Critical : " +
+          scanProcess.severities.critical +
+          " High : " +
+          scanProcess.severities.high +
+          " Medium : " +
+          scanProcess.severities.medium +
+          " Low : " +
+          scanProcess.severities.low +
           "\n"
       );
 
-      const newIssues = await newIssue(repoName, token, ctServer);
-      const weaknessIsKeywords = output.weakness_is.split(",");
-      const weaknessIsCount = findWeaknessTitles(newIssues, weaknessIsKeywords);
+      const weaknessIsCount = findWeaknessTitles(
+        scanProcess.weaknessesArr,
+        output.weakness_is.split(",")
+      );
 
       if (output.condition === "OR") {
         if (
           output.max_number_of_critical &&
-          output.max_number_of_critical <
-            progressSeverity[progressSeverity.length - 1].critical
+          output.max_number_of_critical < scanProcess.severities.critical
         ) {
-          core.setFailed("!! FAILED_ARGS : Critical limit exceeded -- ");
-          scanProcess.data.state === "end";
-          cancellation = true;
+          core.setFailed("!! FAILED_ARGS : Critical limit exceeded.");
+          throw new Error(
+            "Pipeline interrupted because the FAILED_ARGS arguments you entered were found..."
+          );
         } else if (
           output.max_number_of_critical &&
-          output.max_number_of_high <
-            progressSeverity[progressSeverity.length - 1].high
+          output.max_number_of_high < scanProcess.severities.high
         ) {
-          core.setFailed("!! FAILED_ARGS : High limit exceeded -- ");
-          scanProcess.data.state === "end";
-          cancellation = true;
+          core.setFailed("!! FAILED_ARGS : High limit exceeded. ");
+          throw new Error(
+            "Pipeline interrupted because the FAILED_ARGS arguments you entered were found..."
+          );
         } else if (weaknessIsCount.length > 0) {
           core.setFailed(
             "!! FAILED_ARGS : Weaknesses entered in the weakness_is key were found during the scan."
           );
-          scanProcess.data.state === "end";
+          scanProcess.state === "end";
         }
       } else if (output.condition === "AND") {
         if (
           (output.max_number_of_critical &&
-            output.max_number_of_critical <
-              progressSeverity[progressSeverity.length - 1].critical) ||
+            output.max_number_of_critical < scanProcess.severities.critical) ||
           (output.max_number_of_critical &&
-            output.max_number_of_high <
-              progressSeverity[progressSeverity.length - 1].high) ||
+            output.max_number_of_high < scanProcess.severities.high) ||
           weaknessIsCount.length > 0
         ) {
           core.setFailed(
-            "!! FAILED ARGS : Not all conditions are met according to the given arguments"
+            "!! FAILED ARGS : Not all conditions are met according to the given arguments."
           );
-          scanProcess.data.state === "end";
-          cancellation = true;
+          throw new Error(
+            "Pipeline interrupted because the FAILED_ARGS arguments you entered were found..."
+          );
         }
       }
     }
-    if (scanProcess.data.state === "end" || cancellation) {
+    if (scanProcess.state === "end") {
       await resultScan(
-        scanProcess.data.riskscore,
-        scanProcess.data.started_at,
-        scanProcess.data.ended_at,
-        scanProcess.data.severities
+        scanProcess.progress,
+        scanProcess.severities,
+        sid,
+        scanProcess.riskscore,
+        scanProcess.started_at,
+        scanProcess.ended_at
       );
     } else {
       setTimeout(function () {
-        awaitScan(sid);
-      }, 3000);
+        scanStatus(sid);
+      }, 5000);
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 };
-const resultScan = async (riskS, started_at, ended_at, totalSeverities) => {
-  try {
-    let reason;
-    if (!cancellation) {
-      reason = `Scan Completed... %${progressData[progressData.length - 1]}`;
+
+const resultScan = async (progress, severities, sid) => {
+  const reason = `Scan Completed... %${progress}`;
+  core.warning(
+    "Result : " +
+      reason +
+      "- Critical : " +
+      severities.critical +
+      " High : " +
+      severities.high +
+      " Medium : " +
+      severities.medium +
+      " Low : " +
+      severities.low
+  );
+  const report = await result(ctServer, sid, authToken, orgname);
+
+  if (github.context.eventName === "push") {
+    try {
+      await octokit.repos.createCommitComment({
+        owner: repoOwner,
+        repo: repoName,
+        commit_sha: commitId,
+        body: report,
+      });
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+  }
+
+  if (github.context.eventName === "pull_request") {
+    if (output.automerge) {
+      try {
+        await octokit.pulls.update({
+          owner: repoOwner,
+          repo: repoName,
+          pull_number: pr.number,
+          state: "closed",
+        });
+        await octokit.pulls.merge({
+          owner: repoOwner,
+          repo: repoName,
+          pull_number: pr.number,
+        });
+      } catch (error) {
+        core.setFailed(error.message);
+      }
     } else {
-      reason =
-        "Pipeline interrupted because the FAILED_ARGS arguments you entered were found... ";
-    }
-
-    let totalSev = {
-      critical: totalSeverities.critical ? totalSeverities.critical : 0,
-      high: totalSeverities.high ? totalSeverities.high : 0,
-      medium: totalSeverities.medium ? totalSeverities.medium : 0,
-      low: totalSeverities.low ? totalSeverities.low : 0,
-    };
-
-    core.warning(
-      "\n" +
-        "Result : " +
-        reason +
-        "\n" +
-        "Critical : " +
-        totalSev.critical +
-        "\n" +
-        "High : " +
-        totalSev.high +
-        "\n" +
-        "Medium : " +
-        totalSev.medium +
-        "\n" +
-        "Low : " +
-        totalSev.low +
-        "\n"
-    );
-
-    const newIssues = await newIssue(repoName, token, ctServer);
-    const allIssues = await allIssue(repoName, token, ctServer);
-
-    let durationTime = convertToHHMMSS(ended_at, started_at);
-    const riskscore = getScore(riskS);
-
-    const newIssuesData = countAndGroupByTitle(newIssues);
-    const newIssuesSeverity = countBySeverity(newIssuesData);
-    const allIssuesData = countAndGroupByTitle(allIssues);
-    const allIssuesSeverity = countBySeverity(allIssuesData);
-
-    let totalCountNewIssues = 0;
-    for (const obj of newIssuesData) {
-      totalCountNewIssues += obj.count;
-    }
-
-    let html = htmlCode(
-      totalCountNewIssues,
-      newIssuesSeverity,
-      allIssuesData,
-      durationTime,
-      riskS,
-      riskscore,
-      totalSeverities,
-      repoName,
-      ctServer
-    );
-
-    if (pr && pr.number) {
-      if (allIssues.length === 0) {
-        if (output.automerge) {
-          try {
-            await octokit.pulls.update({
-              owner: repoOwner,
-              repo: repoName,
-              pull_number: pr.number,
-              state: "closed",
-            });
-            await octokit.pulls.merge({
-              owner: repoOwner,
-              repo: repoName,
-              pull_number: pr.number,
-            });
-          } catch (error) {
-            core.setFailed(error.message);
-          }
-        } else {
-          try {
-            await octokit.pulls.createReview({
-              owner: repoOwner,
-              repo: repoName,
-              pull_number: pr.number,
-              event: "COMMENT",
-              body: html,
-            });
-          } catch (error) {
-            core.setFailed(error.message);
-          }
-        }
-      } else {
-        const weaknessIsKeywords = output.weakness_is.split(",");
-        const weaknessIsCount = findWeaknessTitles(
-          allIssues,
-          weaknessIsKeywords
-        );
-        if (output.condition === "OR") {
-          if (
-            output.max_number_of_critical &&
-            output.max_number_of_critical < totalSeverities?.critical
-          ) {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed("!! FAILED_ARGS : Critical limit exceeded -- ");
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          } else if (
-            output.max_number_of_critical &&
-            output.max_number_of_high < totalSeverities?.high
-          ) {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed("!! FAILED_ARGS : High limit exceeded -- ");
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          } else if (weaknessIsCount.length > 0) {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed(
-                "!! FAILED_ARGS : Weaknesses entered in the weakness_is key were found during the scan."
-              );
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          } else {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed(
-                "!! A condition you entered in FAILED_ARGS was not found, but there are findings from the scan."
-              );
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          }
-        } else if (output.condition === "AND") {
-          if (
-            (output.max_number_of_critical &&
-              output.max_number_of_critical < totalSeverities?.critical) ||
-            (output.max_number_of_critical &&
-              output.max_number_of_high < totalSeverities?.high) ||
-            weaknessIsCount.length > 0
-          ) {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed(
-                "!! FAILED ARGS : Not all conditions are met according to the given arguments"
-              );
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          } else {
-            try {
-              await octokit.pulls.update({
-                owner: repoOwner,
-                repo: repoName,
-                pull_number: pr.number,
-                state: "COMMENT",
-                body: html,
-              });
-              core.setFailed(
-                "!! A condition you entered in FAILED_ARGS was not found, but there are findings from the scan."
-              );
-            } catch (error) {
-              core.setFailed(error.message);
-            }
-          }
-        }
+      try {
+        await octokit.pulls.createReview({
+          owner: repoOwner,
+          repo: repoName,
+          pull_number: pr.number,
+          event: "COMMENT",
+          body: report,
+        });
+      } catch (error) {
+        core.setFailed(error.message);
       }
     }
-  } catch (error) {
-    core.setFailed(error.message);
   }
 };
 
 (async () => {
-    const start = await startScan();
-    await awaitScan(start.data.scan_id);
+  let start;
+  try {
+    await signIn();
+    checked = await checkProject();
+    if (checked.type === null) await createProject();
+    start = await startScan();
+    await scanStatus(start.data.scan_id);
+  } catch (error) {
+    throw new Error(error);
+  }
 })();
