a54c2d68ebed4e2b80a8fecc3f63c1b176531ab3
Taha Yıldırım <59919927+thyldrm@users.noreply.github.com>
2024-06-27 15:01:45 +0300

    Update q.js

--- q.js
+++ q.js
@@ -3,3 +3,9 @@ console.log(password)
 
 const SECRET_KEY = "123456"
 console.log(SECRET_KEY)
+
+fs.writeFile("user-data.txt", "userName=asd", (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log("wrote file");
+});
